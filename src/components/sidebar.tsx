48ed6b8552fc12c763647d15fe4dc6c7ee54c20f
Avila-Pato <p.avilaf1998@gmail.com>
2025-03-25 00:29:10 -0300

    Remove @next/font dependency and update sidebar component with icons and improved layout

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -1,18 +1,33 @@
 import Link from "next/link";
+import { 
+  FileText, 
+  Settings,
+  LaptopMinimalCheck,
+  PanelRightOpen,
+  PanelBottomOpen,
+  Moon,
+  Sun,
+  } from 'lucide-react';
 
 const Sidebar = () => {
   return (
-    <aside className="w-52 h-full bg-[#e6e7ee] p-4 font-bold text-gray-800 ">
+    <aside className="w-52 h-full bg-[#e6e7ee] p-4 font-bold text-gray-500  " >
       <ul className="space-y-2">
         <li className="text-neutral-600 font-light text-xs">Documentos</li>
-        <li>
+
+        <li className="font-medium">
           <Link href="/dashboard" className="block p-2 hover:bg-gray-400 rounded ">
+          <LaptopMinimalCheck className="inline-flex justify-center items-center mr-2 " size={20}/>
             Dashboard
           </Link>
         </li>
-        <Link href="/dashboard/introduccion" className="block p-2 hover:bg-gray-400 rounded">
-            Introduccion
+
+        <li className="font-medium">
+        <Link href="/dashboard/introduccion" className="block p-2  hover:bg-gray-400 rounded">
+        <FileText className="inline-flex  justify-center items-center mr-2 " size={20} />   
+         Introduccion
           </Link>
+        </li>
             
           {/* de esta manera se manejan las rutas por props */}
           {/* <li>
@@ -21,32 +36,40 @@ const Sidebar = () => {
           </Link>
           */}
 
-          <li>
+          <li className="font-medium">
           <Link href="/dashboard/productos" className="block p-2 hover:bg-gray-400 rounded">
-            Productos
+          < Settings className="inline-flex  justify-center items-center mr-2 " size={20}  />
+            Setup
           </Link>
             </li>
+
               <li className="text-neutral-600 font-light text-xs">Layouts</li>
-            <li>
+
+            <li className="font-medium">
           <Link href="/dashboard/productos" className="block p-2 hover:bg-gray-400 rounded">
+          <PanelRightOpen className="inline-flex  justify-center items-center mr-2 " size={20}/>
             Vertical
           </Link>
             </li>
 
-            <li>
+            <li className="font-medium">
           <Link href="/dashboard/productos" className="block p-2 hover:bg-gray-400 rounded">
+          <PanelBottomOpen className="inline-flex  justify-center items-center mr-2 " size={20}/>
             Horizontal
           </Link>
             </li>
               <li className="text-neutral-600 font-light text-xs">Temas</li>
-            <li>
+
+            <li className="font-medium">
           <Link href="/dashboard/productos" className="block p-2 hover:bg-gray-400 rounded">
+          <Sun className="inline-flex  justify-center items-center mr-2 " size={20}/>
             Version Light
           </Link>
             </li>
 
-            <li>
+            <li className="font-medium">
           <Link href="/dashboard/productos" className="block p-2 hover:bg-gray-400 rounded">
+          <Moon className="inline-flex  justify-center items-center mr-2 " size={20}/>
             Version Dark
           </Link>
             </li>
